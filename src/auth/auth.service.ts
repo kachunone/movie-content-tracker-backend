5df95ba348a9685cc00af27d2076c0bca52cacbc
Morris Wan <102487449+kachunone@users.noreply.github.com>
2023-08-16 00:10:07 -0400

    fixed hashing password problem

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
